0158c99fd0d5193d285269808c50c915b8c32a1a
Harkanwal Singh <hankanallt@gmail.com>
2019-12-08 00:06:20 +0100

    First Commit

---
+++ src/polymorfism/Animal.java
@@ -0,0 +1,16 @@
+
+package polymorfism;
+
+import java.util.ArrayList;
+
+
+public class Animal {
+
+     public void move(){
+            System.out.println("An Animal moves");
+        
+            
+        }
+    }
+
+
